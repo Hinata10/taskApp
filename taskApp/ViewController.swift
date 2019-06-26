70c4eff9fa86227cdcbfe09968f571c8c804221f
Hinata10 <richfennec@hinatasukehakunomacbook-pro.local>
2019-06-26 23:08:46 +0900

    Initial Commit

---
+++ taskApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  taskApp
+//
+//  Created by 日向亮博 on 2019/06/26.
+//  Copyright © 2019 Hinata10. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
