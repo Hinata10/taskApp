1ba37435b98c5fa9979868c91e3f74bcc1d2c427
Hinata10 <shinkai.xzettai.62@icloud.com>
2019-07-04 19:13:30 +0900

    全てのコードを見直した。StoryboardのTableViewのidentifierであるcellをCellに直した。

---
+++ taskApp/Task.swift
@@ -0,0 +1,24 @@
+//
+//  Task.swift
+//  taskApp
+//
+//  Created by 日向亮博 on 2019/07/03.
+//  Copyright © 2019 Hinata10. All rights reserved.
+//
+import Foundation
+import RealmSwift
+
+class Task: Object{
+    //管理用ID、プライマリーキー
+    @objc dynamic var id = 0
+    //タイトル
+    @objc dynamic var title = ""
+    //内容
+    @objc dynamic var contents = ""
+    //日時
+    @objc dynamic var date = Date()
+    //idをプライマリーキーとして設定
+    override static func primaryKey() -> String?{
+        return "id"
+    }
+}
--- taskApp/ViewController.swift
+++ taskApp/ViewController.swift
@@ -7,11 +7,19 @@
 //
 
 import UIKit
+import RealmSwift
+import UserNotifications
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 //UITableViewDelegateとUITableViewDataSourceプロトコルを追加しそれぞれのメソッドを実装する。
     //IBOutlet接続してプロパティ化した。
     @IBOutlet weak var tableView: UITableView!
+    
+    let realm = try! Realm()//Realmのインスタンスを取得
+    //DB内のタスク格納がされるリスト
+    //日付が近い順番でソート（降順）
+    //以降内容をアップデート毎に自動更新
+    var taskArray = try! Realm().objects(Task.self).sorted(byKeyPath: "date", ascending: false)
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,15 +27,42 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         tableView.delegate = self
         tableView.dataSource = self
     }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?){
+        let inputViewController:InputViewController = segue.destination as! InputViewController
+        if segue.identifier == "cellSegue"{
+            let indexPath = self.tableView.indexPathForSelectedRow
+            inputViewController.task = taskArray[indexPath!.row]
+        }else{
+            let task = Task()
+            task.date = Date()
+            let allTasks = realm.objects(Task.self)
+            if allTasks.count != 0{
+                task.id = allTasks.max(ofProperty: "id")! + 1
+            }
+            inputViewController.task = task
+        }
+    }//下に入力画面から戻って来た時にTableViewを更新させるviewWillAppearを記述
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        tableView.reloadData()
+    }
     //MARK: UITableViewDataSourceのプロトコルのメソッドを以下に示す。
     //まずデータの数を返すメソッド(メソッドなのでfuncを使う)
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
-        return 0
+        return taskArray.count
     }
     //次に各セルの内容を返すメソッド
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
         //再利用可能なcellを得る
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+//           以下修正、Cellに値を設定した。
+        let task = taskArray[indexPath.row]
+        cell.textLabel?.text = task.title
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm"
+        let dateString:String = formatter.string(from: task.date)
+        cell.detailTextLabel?.text = dateString
+        
         return cell
     }
     //MARK: UITableViewDelegateプロトコルのメソッド
@@ -37,12 +72,31 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     }
     //セルが削除可能であるエディタブルである事を伝えるメソッド ->editingStylrForRowAt: セルが削除可能
     func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle{
-        return.delete //削除可能にするため.deleteを返す。
+        return .delete //削除可能にするため.deleteを返す。
     }
     //Deleteが押された時に呼ばれるメソッド ->commit: forRowAt: Deleteが押された時ローカル通知キャンセルしデータベースからタスクを削除できる。
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath){
-        
+        if editingStyle == .delete{
+            //削除するタスクを取得する
+            let task = self.taskArray[indexPath.row]
+            //ローカル通知をキャンセルする。
+            let center = UNUserNotificationCenter.current()
+            center.removePendingNotificationRequests(withIdentifiers: [String(task.id)])
+            //データベースから削除する
+            try! realm.write{
+                self.realm.delete(self.taskArray[indexPath.row])
+                self.realm.delete(task)
+                tableView.deleteRows(at: [indexPath], with: .fade)
+            }
+            //未通知のローカル通知一覧をログ出力
+            center.getPendingNotificationRequests{(requests: [UNNotificationRequest]) in
+                for request in requests{
+                    print("/------------/")
+                    print(request)
+                    print("/------------/")
+                }
+            }
+        }
     }
-
 }
 
